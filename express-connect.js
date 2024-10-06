e41bc3bef6fd6e00266679bf047cea8f1508d692
EthanShen <ethanshen4work@gmail.com>
2024-10-06 10:18:18 +0800

    feat: add static html file use express router to response request

--- express-connect.js
+++ express-connect.js
@@ -32,6 +32,21 @@ const http = require("http");
 
 const express = require("express");
 const app = express();
+const path = require("path");
+// i5ting的教程里面有open模块 但是open模块已经不再支持cjs 只支持es
+// 设置静态文件目录为当前目录下的 dist 文件夹。
+// __dirname 是 nodejs 内置的全局变量，表示当前执行脚本所在的目录。
+// express.static 是一个中间件，用于处理静态文件请求。
+// app.use(express.static(path.join(__dirname, "dist")));
+
+const router = express.Router();
+router.use(express.static(path.join(__dirname, "dist")));
+router.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+// 添加静态路由的router
+app.use(router); // Add this line to use the router
 
 app.get("/add", (req, res) => {
   res.end("res from add");
@@ -50,3 +65,9 @@ app.get((req, res) => {
 });
 
 app.listen(3000);
+// open("http://127.0.0.1:4001");
+// const PORT = 3001;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//     open(`http://127.0.0.1:${PORT}`);
+// });
